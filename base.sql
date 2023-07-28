4b9d44523d871ab48ebbcc6a96da19f6b815bc96
Jocelyn2128 <mami2003nomenjanahary@gmail.com>
2023-07-28 11:26:50 +0300

    table clients-Bouquets

--- base.sql
+++ base.sql
@@ -1,1 +1,19 @@
 --nom de la base de donnée : canal
+
+create table Clients
+(
+    idClients SERIAL PRIMARY KEY not null,
+    nom varchar(60) not null,
+    tel int not null,
+    cin int not null,
+    adresse varchar(100)
+);
+
+create table Bouquets
+(
+    idBouquets serial primary key not null,
+    nomBouquet varchar(100),
+    montant float,
+    idCB int not null,
+    foreign key idCB references Compo_Bouquet(idCB);
+);
