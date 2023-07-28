93208b061a65d1bcf0ac82a67ed703bb79d9c946
Jocelyn2128 <mami2003nomenjanahary@gmail.com>
2023-07-28 11:35:23 +0300

    v2 table

--- base.sql
+++ base.sql
@@ -1,5 +1,6 @@
 --nom de la base de donnée : canal
 
+<<<<<<< HEAD
 create table Clients
 (
     idClients SERIAL PRIMARY KEY not null,
@@ -16,3 +17,20 @@ create table Bouquets
     montant float,
     idCB int not null,
     foreign key idCB references Compo_Bouquet(idCB);
+);
+=======
+create table categorie_chaine
+(
+    idCatChaine serial primary key,
+    categorie varchar(100)
+);
+
+create table chaine
+(
+    idChaine serial primary key,
+    idCatChaine int not null,
+    chaine varchar(50)
+);
+
+alter table chaine add foreign key (idCatChaine) references categorie_chaine (idCatChaine);
+>>>>>>> 4d54f31928c35c9e0a568309c64d99b9b5479629
