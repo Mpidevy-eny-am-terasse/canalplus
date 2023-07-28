5a020f0b4a77ae7dc3a144e0ec4dc39e7b814aa2
Santatra-Randria <rsantatra21@gmail.com>
2023-07-28 14:12:50 +0200

    insert clients

--- base.sql
+++ base.sql
@@ -7,7 +7,8 @@ create table Clients
     nom varchar(60) not null,
     tel int not null,
     cin int not null,
-    adresse varchar(100)
+    adresse varchar(100),
+    mail varchar(35)
 );
 
 create table Bouquets
@@ -62,5 +63,11 @@ create table ChaineCB
     foreign key (idCB) references Compo_Bouquet(idCB),
     foreign key (idChaine) references Chaine(idChaine)
 );
+
+insert into Clients(nom,tel,cin,adresse,mail) values('Papa Lee',0340000110,123654789,'IVH 015 B Talatamaty','lee@gmail.com');
+insert into Clients(nom,tel,cin,adresse,mail) values('Rudy Lee',0331200110,123654700,'IVH 111 B Talatamaty','rudy@gmail.com');
+insert into Clients(nom,tel,cin,adresse,mail) values('Anrick Larsen',0340000021,105654789,'IVB 096 B Talatamaty','anrick@gmail.com');
+insert into Clients(nom,tel,cin,adresse,mail) values('Jary Nomena',0327500110,123654789,'IVA 214 A Talatamaty','jary@gmail.com');
+insert into Clients(nom,tel,cin,adresse,mail) values('Gasy Kely',0348609151,000654789,'IVB 118 A Soavimasoandro','gasy@gmail.com');
 >>>>>>> 4d54f31928c35c9e0a568309c64d99b9b5479629
 
