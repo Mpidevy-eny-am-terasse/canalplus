4d54f31928c35c9e0a568309c64d99b9b5479629
Santatra-Randria <rsantatra21@gmail.com>
2023-07-28 12:37:27 +0200

    creation table

--- base.sql
+++ base.sql
@@ -1,1 +1,16 @@
 --nom de la base de donnée : canal
+
+create table categorie_chaine
+(
+    idCatChaine serial primary key,
+    categorie varchar(100)
+);
+
+create table chaine
+(
+    idChaine serial primary key,
+    idCatChaine int not null,
+    chaine varchar(50)
+);
+
+alter table chaine add foreign key (idCatChaine) references categorie_chaine (idCatChaine);
